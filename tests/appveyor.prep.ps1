e1ac9c8be27bbef1a9bea4f1e7fd45e93662e65d
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-10-04 08:58:19 -0500

    Pester install-module

--- tests/appveyor.prep.ps1
+++ tests/appveyor.prep.ps1
@@ -13,7 +13,7 @@ Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck | Out-Null
 
 #Get Pester (to run tests)
 Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
-choco install pester | Out-Null
+Install-Module -Name Pester -Force -SkipPublisherCheck | Out-Null
 
 #Get opencover.portable (to run DLL tests)
 Write-Host -Object "appveyor.prep: Install opencover.portable" -ForegroundColor DarkGreen
